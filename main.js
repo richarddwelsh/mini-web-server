897a1d719e8560f2982dbdd61b62a107cae1389d
Richard Welsh <richard@welshdesign.co.uk>
2018-01-26 22:32:12 +0000

    freegle mjml tool WIP

--- main.js
+++ main.js
@@ -3,6 +3,11 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.text({type: "text/xml"})); // body-parser will treat incoming text/xml bodies as plain text
+
+var mjml = require('mjml');
+
 var staticRoot = __dirname + "/../webroot"
 var port = 8001
 
@@ -17,8 +22,24 @@ app.get('/', function (req, res) {
   res.send('Serving from' + staticRoot)
 })
 
+// POST handler to translate MJML to HTML (takes XML; returns JSON object)
+app.post('/Mjml2Html', function (req, res) {
+	var incomingMjml = req.body;
+	var resultingHtml;
+	
+	try {
+		resultingHtml = mjml.mjml2html(incomingMjml, {level: "soft"});
+	}
+	catch(e)
+	{
+		res.send({errors: [e.message]});
+	}
+
+	res.send(resultingHtml);
+})
+
 app.use(function(req, res, next){
-	console.log('%s %s', req.method, req.url);
+	// console.log('%s %s', req.method, req.url);
 	next();
 });
 
